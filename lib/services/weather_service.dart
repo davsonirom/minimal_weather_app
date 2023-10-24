fe7074b87cc54ec1fab54a28864bf25857ac773e
Davson Oldac Damasceno Machado <davsonirom@me.com>
2023-10-24 10:09:50 -0300

    corrigifdo erro sintaxe da chamada na API

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,8 +6,8 @@ import 'package:http/http.dart' as http;
 import 'package:minimal_weather_app/model/weather_model.dart';
 
 class WeatherService {
-  //!  https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-  //! https://api.openweathermap.org/data/2.5/weather?q=janauba&appid=key
+  //! 3.0 >  https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+  //! 2.5 > https://api.openweathermap.org/data/2.5/weather?q=cidade&appid=key&unite=metric
   static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
   WeatherService({
@@ -15,13 +15,14 @@ class WeatherService {
   });
 
   Future<WeatherModel> tempoParaHoje(String cidadeNome) async {
-    final response =
-        await http.get(Uri.parse('$BASE_URL=$cidadeNome&appid=$apiKey'));
+    final response = await http
+        .get(Uri.parse('$BASE_URL?q=$cidadeNome&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
       return WeatherModel.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Ops! conexão com marte perdida, API fora de alcançe');
+      throw Exception(
+          'Ops! conexão com marte perdida, API fora de alcançe status = ${response.statusCode}');
     }
   }
 
